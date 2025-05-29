69db130715b234e37f5e3ce369c6ead3109f2943
Adaneazanaw <adaneazanaw74@gmail.com>
2025-05-29 21:51:18 +0300

    Adane change method

--- Librarian.java
+++ Librarian.java
@@ -1,12 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Librarian {
 
-  private String name;
+    private String name;
+    private int employeeId;
+    private List<Book> books;
 
-  private integer employeeId;
+    public Librarian(String name, int employeeId) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.books = new ArrayList<>();
+    }
 
-  public void addBooks(Book b) {
-  }
-  public void removeBook(Book b) {
-  }
+    public void addBook(Book b) {
+        books.add(b);
+        System.out.println("Book added: " + b.getTitle());
+    }
 
+    public void removeBook(Book b) {
+        if (books.remove(b)) {
+            System.out.println("Book removed: " + b.getTitle());
+        } else {
+            System.out.println("Book not found.");
+        }
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
 }
