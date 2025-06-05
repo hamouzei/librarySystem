78b184b385552c5839d0785d394abfc40d263043
hamouzei <hamuzy07@gmail.com>
2025-06-06 02:17:50 +0300

    Hamza added Author method

--- Author.java
+++ Author.java
@@ -2,22 +2,53 @@ import java.util.Vector;
 
 public class Author {
 
-  private String name;
-
-  private String biography;
+    private String name;
+    private String biography;
 
     /**
-   * 
-   * @element-type Book
-   */
-  public Vector  myBook;
+     * List of books written by the author
+     */
+    public Vector<Book> myBook;
 
-  public String getName() {
-  return null;
-  }
+    // Constructor
+    public Author(String name, String biography) {
+        this.name = name;
+        this.biography = biography;
+        this.myBook = new Vector<>();
+    }
 
-  public String getBiography() {
-  return null;
-  }
+    // Getter for name
+    public String getName() {
+        return name;
+    }
 
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for biography
+    public String getBiography() {
+        return biography;
+    }
+
+    // Setter for biography
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    // Add a book to author's book list
+    public void addBook(Book book) {
+        myBook.add(book);
+    }
+
+    // Remove a book
+    public void removeBook(Book book) {
+        myBook.remove(book);
+    }
+
+    // Get list of all books by the author
+    public Vector<Book> getBooks() {
+        return myBook;
+    }
 }
